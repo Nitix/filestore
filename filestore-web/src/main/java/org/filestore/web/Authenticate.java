301856948117e70238db02ef2e852c343fc9c677
MarintheJerome <jerome.marinthe@gmail.com>
2016-12-15 11:57:30 +0100

    reparation github redirect

--- filestore-web/src/main/java/org/filestore/web/Authenticate.java
+++ filestore-web/src/main/java/org/filestore/web/Authenticate.java
@@ -4,6 +4,7 @@ import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.URLConnectionClient;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.GitHubTokenResponse;
+import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
 import org.apache.oltu.oauth2.common.OAuthProviderType;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
@@ -73,9 +74,15 @@ public class Authenticate {
             OAuthClient oAuthClient = new OAuthClient(new URLConnectionClient());
             OAuthClientRequest request = providerObject.createTokenRequest(code);
 
-            GitHubTokenResponse oAuthResponse = oAuthClient.accessToken(request, GitHubTokenResponse.class);
-
-            String accessToken = oAuthResponse.getAccessToken();
+            String accessToken = null;
+            if(provider.equals("github")){
+                GitHubTokenResponse gitoAuthResponse = oAuthClient.accessToken(request, GitHubTokenResponse.class);
+                accessToken = gitoAuthResponse.getAccessToken();
+            }
+            else{
+                OAuthJSONAccessTokenResponse oAuthResponse = oAuthClient.accessToken(request);
+                accessToken = oAuthResponse.getAccessToken();
+            }
             //long expiresIn = oAuthResponse.getExpiresIn();
 
             session.setAttribute("token-value", accessToken);
